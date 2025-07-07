1823eff0983b429c69a7963e38460b0970b8a0c4
Utakata1024 <tty1024kit@gmail.com>
2025-07-07 10:21:10 +0900

    feat/FooterPage

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white p-8 mt-12">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center md:items-start gap-8">
+        {/* 左側 */}
+        <div>
+            aaa
+        </div>
+        {/* 中間 */}
+        <div>
+            bbb
+        </div>
+        {/* 右側 */}
+        <div>
+            ccc
+        </div>
+      </div>
+    </footer>
+  );
+}
