9c19418c43cce48375e37e8fa4e28fe11b00b7fb
Utakata1024 <tty1024kit@gmail.com>
2025-07-07 14:58:20 +0900

    feat/Login: firebaseによるログイン機能の追加

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,19 +1,42 @@
 "use client";
 
 import { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/lib/firebase";
+import { useRouter } from "next/navigation";
+import { sign } from "crypto";
 
 export default function LoginForm() {
   // ユーザーの入力値のための状態変数定義
   const [id, setId] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState<string | null>(null); // エラーメッセージ用
+  const router = useRouter(); // useRouterフックの初期化
 
   // フォーム送信時の処理
-  // あとで実装
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError(null); // エラーメッセージをクリア
+
+    try {
+      await signInWithEmailAndPassword(auth, id, password);
+      router.push("/schedule"); // 成功したらスケジュールページへ
+    } catch (err: any) {
+      console.log("ログインエラー:", err);
+      if (err.code === 'auth/invalid-credential') {
+        setError('idまたはパスワードが間違っています。')
+      } else if (err.code === 'auth/user-disabled') {
+        setError('このアカウントは無効化されています。')
+      } else {
+        setError('ログイン中に予期せぬエラーが発生しました。もう一度お試しください。');
+      }
+    }
+  }
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100 p-4">
       <form
-        // onSubmit={}
+        onSubmit={handleSubmit}
         className="flex flex-col gap-4 p-8 border rounded-lg shadow-xl max-w-sm w-full bg-white"
       >
         <h2 className="text-3xl font-bold text-center text-gray-800 mb-6">
@@ -51,6 +74,8 @@ export default function LoginForm() {
             ログイン
           </button>
         </div>
+        {/* エラーメッセージがあれば表示 */}
+        {error && <p className="text-red-500 text-sm text-center">{error}</p>}
         <div>
           <p className="text-center text-sm text-gray-600 mt-4">
             アカウントをお持ちでないですか？
