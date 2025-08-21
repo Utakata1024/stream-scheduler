68bdf03e6693d6fb6504c3d8aead8f04fa321c30
Utakata1024 <tty1024kit@gmail.com>
2025-08-21 12:53:41 +0900

    update:チャンネル入力欄改善

--- src/components/channels/AddChannelForm.tsx
+++ src/components/channels/AddChannelForm.tsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 
 interface AddChannelFormProps {
   onAddChannel: (
-    channelId: string,
+    newChannelInput: string,
     resetInput: (id: string) => void
   ) => Promise<void>;
   addingChannel: boolean;
@@ -14,11 +14,11 @@ export default function AddChannelForm({
   onAddChannel,
   addingChannel,
 }: AddChannelFormProps) {
-  const [newChannelId, setNewChannelId] = useState("");
+  const [newChannelInput, setNewChannelInput] = useState("");
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    await onAddChannel(newChannelId, setNewChannelId);
+    await onAddChannel(newChannelInput, setNewChannelInput);
   };
 
   return (
@@ -30,9 +30,9 @@ export default function AddChannelForm({
         <input
           id="new-channel-id"
           type="text"
-          value={newChannelId}
-          onChange={(e) => setNewChannelId(e.target.value)}
-          placeholder="YouTubeチャンネルID"
+          value={newChannelInput}
+          onChange={(e) => setNewChannelInput(e.target.value)}
+          placeholder="YouTubeチャンネルIDまたはTwitchユーザー名を入力"
           className="flex-grow px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
           disabled={addingChannel}
           required
@@ -46,9 +46,12 @@ export default function AddChannelForm({
         </button>
       </form>
       <p className="text-sm text-gray-500 mt-3 text-center">
-        ※チャンネルIDはYouTubeチャンネルのURLから取得できます。
+        ※YouTubeチャンネルIDはチャンネルのURLから取得できます。
         (例:youtube.com/channel/
         <span className="underline decoration-red-500">UC...</span>)
+        ※Twitchユーザー名はチャンネルのURLから取得できます。
+        (例:twitch.tv/
+        <span className="underline decoration-red-500">...</span>)
       </p>
     </div>
   );
